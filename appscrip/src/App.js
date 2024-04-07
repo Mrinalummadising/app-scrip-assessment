0c1dffa6213fb5223341e9fee3291f5e8294dec4
mrinalummadising <mrinalummadising@gamil.com>
2024-04-07 21:06:15 +0530

    app-scrip

---
+++ appscrip/src/App.js
@@ -0,0 +1,26 @@
+
+import { RouterProvider, Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+import ReactRouter from "./components/ReactRouter";
+
+const RenderLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+function App() {
+  const router = ReactRouter({ RenderLayout });
+  return (
+    <div className="app-main-container">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
